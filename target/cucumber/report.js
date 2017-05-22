4d552984b5f72d3a4292e08522ef2fd8b586d517
willmcb <willmcbgithub@gmail.com>
2017-05-22 17:09:45 +0100

    initial commit

---
+++ target/cucumber/report.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri('Annotation/annotation.feature');
+formatter.feature({
+  "line": 1,
+  "name": "annotation",
+  "description": "",
+  "id": "annotation",
+  "keyword": "Feature"
+});
+});
